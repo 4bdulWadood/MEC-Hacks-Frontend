c19280b276a0d1bf0b278090d64754ab3f7f3507
4bdulWadood <a1syed@torontomu.ca>
2023-12-27 02:59:28 -0500

    Testing Webhook

--- mec-hacks/src/App.js
+++ mec-hacks/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Testing WebHook!
+          Testing Webhook!
         </p>
         <a
           className="App-link"
